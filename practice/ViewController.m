97429dc8cb5c6a179d32c19a2328be547a9cd9d3
Terry Bu <burea1124@gmail.com>
2015-03-20 12:27:54 -0400

    initial

--- practice/ViewController.m
+++ practice/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (nonatomic) float scaleFactor;
+@property (nonatomic) float angle;
+@property (strong, nonatomic) UIView *boxView;
+
 @end
 
 @implementation ViewController
@@ -17,6 +21,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _scaleFactor = 2;
+    _angle = 180;
+    CGRect frameRect = CGRectMake(10, 10, 45, 45);
+    _boxView = [[UIView alloc] initWithFrame:frameRect];
+    _boxView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:_boxView];
+    
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self.view];
+    
+    [UIView animateWithDuration:2.0
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         CGAffineTransform scaleTrans =
+                         CGAffineTransformMakeScale(_scaleFactor, _scaleFactor);
+                         
+                         CGAffineTransform rotateTrans =
+                         CGAffineTransformMakeRotation(_angle * M_PI / 180);
+                         
+                         _boxView.transform = CGAffineTransformConcat(scaleTrans,
+                                                                      rotateTrans);
+                         _angle = (_angle == 180 ? 360 : 180);
+                         _scaleFactor = (_scaleFactor == 2 ? 1 : 2);
+                         _boxView.center = location;
+                     } completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
